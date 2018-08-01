9fff1e607e1208a326f2ede354aec612f863b1f8
gavinw <gavinwiener@gmail.com>
2018-08-01 21:38:41 +0700

    Import the module rather

--- s3_script.py
+++ s3_script.py
@@ -3,6 +3,7 @@ import traceback
 import yaml
 import boto3
 import botocore
+from s3_aws_client import S3Client
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -20,41 +21,9 @@ AWS_SECRET_ACCESS_KEY = config['aws_credentials']['aws_secret_key_id']
 S3_HOST = config['s3']['aws_s3_host']
 S3_REGION = config['aws_credentials']['aws_region']
 
-session = boto3.Session(
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-    region_name=S3_REGION,
-)
+client = S3Client(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, S3_HOST, S3_REGION)
 
-s3 = session.resource('s3')
+filename = 'temporary.txt'
 
-
-def download_file_from_bucket(bucket, filepath):
-    """
-    Download a file relative to a bucket
-    File-path must include a leading forward slash
-    :param bucket:
-    :param filepath:
-    :return:
-    """
-    try:
-        s3.Bucket(bucket).download_file(filepath, 'bloop.csv')
-    except botocore.exceptions.ClientError as e:
-        if e.response['Error']['Code'] == "404":
-            print("The object does not exist.")
-        else:
-            raise
-
-
-def upload_file_to_bucket(bucket, local_file, destination):
-
-    try:
-        data = open(local_file, 'rb')
-        s3.Bucket(bucket).put_object(Key=destination, Body=data)
-    except botocore.exceptions.ClientError as e:
-        traceback.print_exc()
-
-
-upload_file_to_bucket('tupal', 'config_template.yml', 'config_template.yml')
-upload_file_to_bucket('tupal', 'config_template.yml', 'blip/config_template.yml')
+client.upload_file_to_bucket('tupal', filename , filename)
 
